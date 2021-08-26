6bf5c042417cd8f696af127f7e6467a9e28519e1
Aaron Textor <atextor@lakecountyfl.gov>
2021-08-26 11:53:48 -0400

    Update data-hideafter.js

--- data-hideafter.js
+++ data-hideafter.js
@@ -1,6 +1,6 @@
 window.onload = function () {
 	let elementsToHide = document.querySelectorAll('[data-hideafter]'),  //  Gets elements that need to be hidden based on the "data-hideafter" attribute.
-    	currentDate = new Date().getTime();  //  Gets current date and time.
+		currentDate = new Date().getTime();  //  Gets current date and time.
 
 	var visibleElementCount = 0;  //  How many elements are visible?
 
@@ -22,19 +22,15 @@ window.onload = function () {
 	if (visibleElementCount == 0) {  //  If "visibleElementCount" == 0, do something.
 		if (document.getElementsByClassName('noElementsVisibleStatement')) {  //  If there are "noElementsVisibleStatement"s that exist, do something.
 			var noElementsVisibleStatement = document.getElementsByClassName('noElementsVisibleStatement');  //  Assign variable name.
-			
 			for (let j = 0; j < noElementsVisibleStatement.length; j++) {  //  For each "noElementsVisibleStatement," do something.
-				let statement = noElementsVisibleStatement[j].classList;  //  Assign variable name.
-				statement.remove('hidden');  //  Remove "hidden" CSS class.
+				noElementsVisibleStatement[j].classList.remove('hidden');  //  Remove "hidden" CSS class.
 			}
 		}
 	} else {  //  If "visibleElementCount" != 0, do something.
 		if (document.getElementsByClassName('elementsVisibleStatement')) {  //  If there are "elementsVisibleStatement"s that exist, do something.
 			var elementsVisibleStatement = document.getElementsByClassName('elementsVisibleStatement');  //  Assign variable name.
-			
 			for (let k = 0; k < elementsVisibleStatement.length; k++) {  //  For each "elementsVisibleStatement," do something.
-				let statement = elementsVisibleStatement[k].classList;  //  Assign variable name.
-				statement.remove('hidden');  //  Remove "hidden" CSS class.
+				elementsVisibleStatement[k].classList.remove('hidden');  //  Remove "hidden" CSS class.
 			}
 		}
 	}
@@ -42,7 +38,6 @@ window.onload = function () {
 
 	//  After finishing up, turn off the loader.
 	if (document.getElementById("hiddenElementsLoader")) {  //  If "hiddenElementsLoader" exists, do something.
-		var loader = document.getElementById("hiddenElementsLoader").classList;  //  Assign variable name.
-		loader.add("hidden");  //  Add "closed" CSS class.
+		document.getElementById("hiddenElementsLoader").classList.add("hidden");  //  Add "closed" CSS class.
 	}
 }
